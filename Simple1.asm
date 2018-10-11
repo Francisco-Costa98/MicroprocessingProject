f67e7c9a4f0e920db9951131ef6728f298a18858
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-11 15:48:20 +0100

    added cascading delay

--- Simple1.asm
+++ Simple1.asm
@@ -2,6 +2,36 @@
 	
 	code
 	org 0x0
+	goto	start
+	
+	org 0x100		    ; Main code starts here at address 0x100
+
+start
+	movlw   0xFF
+	movwf   0x20, ACCESS
+	movwf   0x21, ACCESS
+	movwf   0x22, ACCESS
+	movwf	TRISD, ACCESS
+	movlw 	0x0
+	movwf	TRISC, ACCESS	    ; Port C all outputs
+	bra 	test
+loop	movff 	0x06, PORTC
+	incf 	0x06, W, ACCESS
+test	movwf	0x06, ACCESS	    ; Test for end of loop condition
+	movf PORTD, W, ACCESS
+	cpfsgt 	0x06, ACCESS
+	call	delay
+	bra 	loop	; Not yet finished goto start of loop again
+	
+delay	decfsz 0x20 ; decrement until zero
+	bra delay
+	call delay2	
+	return
+	
+delay2	decfsz 0x21 ; decrement until zero	
+	bra delay
+	call delay3
+	return
 	goto	setup
 		
 SPI_MasterInit ; Set Clock edge to positive
