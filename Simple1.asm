9a17d787c29a47843a2f0c9938b994ef7b4f871f
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-11 15:35:40 +0100

    made D an input port, implemented cascading delay

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,30 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw   0xFF
+	movwf   0x20, ACCESS
+	movwf   0x21, ACCESS
+	movwf	TRISD, ACCESS
 	movlw 	0x0
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x06, PORTC
 	incf 	0x06, W, ACCESS
 test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0xFF
+	movf PORTD, W, ACCESS
 	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
+	call	delay
+	bra 	loop	; Not yet finished goto start of loop again
+	
+delay	decfsz 0x20 ; decrement until zero
+	bra delay
+	call delay2	
+	return
+	
+delay2	decfsz 0x21 ; decrement until zero	
+	bra delay
+	return
+		
 	goto 	0x0		    ; Re-run program from start
 	
 	end
