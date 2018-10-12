faa124a9af3711c07fd7077cb449c2269b3a15a0
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-12 11:20:47 +0100

    made pattern work

--- Simple1.asm
+++ Simple1.asm
@@ -14,11 +14,11 @@ setup	bcf	EECON1, CFGS	; point to Flash program memory
 	movlw	0xFF
 	movwf	0x20, ACCESS
 	movwf	0x21, ACCESS
-	movlw	0xAA
+	movlw	0xFF
 	movwf	0x22, ACCESS
 	goto	start
 	; ******* My data and where to put it in RAM *
-myTable db	$17, $51, $119, $255, $238, $204, $136
+myTable db	0x00, 0x11, 0x33, 0x77, 0xFF, 0xEE, 0xCC, 0x88, 0x00, 0x11, 0x33, 0x77, 0xFF, 0xEE, 0xCC, 0x88, 0x00
 	constant 	myArray=0x400	; Address in RAM for data
 	constant 	counter=0x10	; Address of counter variable
 	; ******* Main programme *********************
@@ -29,12 +29,12 @@ start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM
 	movwf	TBLPTRH		; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.22		; 22 bytes to read
+	movlw	.15		;22 bytes to read
 	movwf 	counter		; our counter register
 loop 	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
 	movff	INDF0, PORTC
-	call delay
+	call	delay
 	decfsz	counter		; count down to zero
 	bra	loop		; keep going until finished
 	
@@ -45,12 +45,12 @@ delay	decfsz 0x20 ; decrement until zero
 	
 	
 delay1	decfsz 0x21 ; decrement until zero
-	bra delay1
+	bra delay
 	call delay2
 	return
 	
 delay2	decfsz 0x22 ; decrement until zero
-	bra delay2
+	bra delay1
 	return
 	goto	0
 
