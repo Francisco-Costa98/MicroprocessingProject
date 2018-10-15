9767db368e27edb6f326ef15cee485e5fe57268a
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-15 14:53:09 +0100

    set up starting code for output and input ports

--- Simple1.asm
+++ Simple1.asm
@@ -2,17 +2,25 @@
 	
 	code
 	org 0x0
-	goto	start
+	goto	setup
 		
-start	movlw	0x00
-	movwf	TRISD, ACCESS
-	movwf	TRISC, ACCESS
-	movlw	0x45
-	movwf	PORTC, ACCESS
-	movlw	0x00
-	movwf	PORTD, ACCESS
-	movlw	0x01
-	movwf	PORTD, ACCESS
+setup	movlw	0x00
+	movwf	TRISD, ACCESS	;sets port d to input port
+	movlw	0xC3		; turns all control lines to high
+	movwf	PORTD, ACCESS	; moves high values to port D
+	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
+	bsf PADCFG1, REPU, BANKED ; PortE pull-ups on
+	movlb 0x00 ; set BSR back to Bank 0
+	setf TRISE ; Tri-state PortE
+	goto start
+	
+	
+start
+	
+	
+write1	movlw	0x20		; only turns on oe 1 for writting
+	movwf	PORTD, ACCESS	;moves value for high oe1 to port D
+	
 	
 	goto 0x0
 	end
