56785260b1139aac73ddf37476aae2e1e141a6db
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-18 15:14:53 +0100

    code saves two 8 bit values and sends them to different ports

--- Simple1.asm
+++ Simple1.asm
@@ -10,6 +10,7 @@ setup	movlw   0x04
 	movwf	TRISD, ACCESS	;sets port d to input port
 	movlw	0x00
 	movwf	TRISC, ACCESS	;sets port c to input port
+	movwf	TRISF, ACCESS	;sets port h to input
 	movlw	0xC3		; turns all control lines to high
 	movwf	PORTD, ACCESS	; moves high values to port D
 	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
@@ -23,9 +24,12 @@ start	movlw   0x04
 	movwf   0x20, ACCESS	;delay register
 	call write1
 	call read
+	movlw   0x04
+	movwf   0x20, ACCESS
+	movff	PORTE, PORTC
 	call write2
 	call read2
-	movff	PORTE, PORTC
+	movff	PORTE, PORTF
 	goto start
 	
 	
@@ -53,10 +57,10 @@ delay	decfsz 0x20 ; decrement until zero
 	bra delay	
 	return	
 
-	write2	movlw	0x82		; only turns on oe 2 and clock2 for writting
+write2	movlw	0x82		; only turns on oe 2 and clock2 for writting
 	movwf	PORTD, ACCESS	;moves value for high oe1 to port D
 	clrf	TRISE		;sets PORTE to outputs
-	movlw	0x11		; chose random number to send to porte
+	movlw	0x11		; chose random number to send to port e
 	movwf	PORTE, ACCESS
 	movlw	0x80		;makes clock tick
 	movwf	PORTD, ACCESS	;makes clock tick
