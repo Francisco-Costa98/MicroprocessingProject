2a060f1478cda1a995e6a3abc8eaaa9021ea0d37
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-22 16:51:59 +0100

    setup

--- Simple1.asm
+++ Simple1.asm
@@ -13,101 +13,33 @@ myArray res 0x80    ; reserve 128 bytes for message data
 rst	code	0    ; reset vector
 	goto	setup
 
-pdata	code    ; a section of programme memory for storing data
-	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
-	constant    myTable_l=.13	; length of data
-	
-main	code
-	; ******* Programme FLASH read Setup Code ***********************
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup LCD
-	movlw	0xFF
-	movwf	TRISD, ACCESS	;set up portd as input
+
+setup	
+	banksel PADCFG1 ; PADCFG1 is not in Access Bank!!
+	bsf	PADCFG1, REPU, BANKED ; PortE pull-ups on
+	movlb	0x00 ; set BSR back to Bank 0
+	setf	TRISE, ACCESS ; Tri-state PortE
+	movlw	0x0F
+	movwf	TRISE, ACCESS
+	clrf	LATE
 	goto	start
 	
-	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+start
+	movff	PORTE,0x01  
 	
-	call	LCD_Clear
-	call	LCD_Cursor_D
-	call	LCD_Cursor_R
-	movlw	myTable_l-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, myArray
-	call	LCD_Write_Message
-	
-	
-	movlw	myTable_l	; output message to UART
-	lfsr	FSR2, myArray
-	call	UART_Transmit_Message
-	
-	;call	clr_loop
-	call Bttn_press 
-	movlw	myTable_l-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, myArray
-	call	LCD_Write_Message
+	bra start
 	
 	goto	$		; goto current line in code
 	
 	
 	
-clr_loop 
-	movlw	0x00
-	movwf	0x01, ACCESS
-	movf	PORTD, W, ACCESS
-	cpfseq 	0x01, ACCESS
-	call	LCD_Clear
-	bra	clr_loop
-	return
 
 	; a delay subroutine if you need one, times around loop in delay_count
 delay	decfsz	delay_count	; decrement until zero
 	bra delay
 	return
 
-
-button_press_message 	
-	call	LCD_Clear
-myTable2 data	"send nudes  \n"
-	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable2)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable2)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable2)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
-	return
+    End
 		
-	
-	
-Bttn_press 
-	movlw	0x01
-	movwf	0x01, ACCESS
-	movf	PORTD, W, ACCESS
-	btfsc 	0x01, ACCESS
-	call button_press_message
-	bra	Bttn_press
-	return
-	end
 
 	
