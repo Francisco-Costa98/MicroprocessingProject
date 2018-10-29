c877601dc0705926782ec255bd655f35b3df861e
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-10-29 10:39:39 +0000

    set up code for two bytes reserved for hex values

--- Simple1.asm
+++ Simple1.asm
@@ -1,13 +1,14 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message, LCD_Write_Hex   ; external LCD subroutines
+	extern  LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Clear, LCD_Cursor_Go_Home  ; external LCD subroutines
 	extern	ADC_Setup, ADC_Read ; external ADC sub routines
     
 	
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
 delay_count res 1   ; reserve one byte for counter in the delay routine
+hex1	    res	2   ; reserve two bytes for hex values
 
 tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
 myArray res 0x80    ; reserve 128 bytes for message data
@@ -21,8 +22,6 @@ main	code
 setup	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup LCD
 	call	ADC_Setup	; setup ADC
-	movlw	0x00
-	movwf	TRISA, ACCESS	;set up porta as output
 	goto	start
 	
 	; ******* Main programme ****************************************
@@ -35,6 +34,7 @@ measure_loop
 	call	LCD_Write_Hex
 	movf	ADRESL,W
 	call	LCD_Write_Hex
+	call	LCD_Cursor_Go_Home
 	goto	measure_loop		; goto current line in code
 
 	; a delay subroutine if you need one, times around loop in delay_count
