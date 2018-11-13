02cb21251c7352151a76bd64d3d1943d7b76bd38
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-13 10:53:39 +0000

    set up interpolate file

---
+++ interpolate.asm
@@ -0,0 +1,19 @@
+#include p18f87k22.inc
+	
+	extern  khigh, klow, myTable
+
+acs0    udata_acs		    ; reserves space for variables used
+	pointer1    res 1
+	pointer2    res 2
+rst	code	0		    ; reset vector
+	goto	setup		    ; goes to code setup
+	
+main	code
+	
+setup
+	
+	
+Interpolate 
+
+
+locate
