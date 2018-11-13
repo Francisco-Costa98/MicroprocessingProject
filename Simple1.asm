48d1228f2de0f5cc7d87af5cb5f1b85c192dea1d
fc916 <fc916@skbl-422-006.ph.ic.ac.uk>
2018-11-13 15:54:08 +0000

    song set up

--- Simple1.asm
+++ Simple1.asm
@@ -1,10 +1,11 @@
 	#include p18f87k22.inc
 	
-	extern  setup_keypad, keypad_start, khigh, klow, test
+	extern  setup_keypad, keypad_start, khigh, klow, test, play_song, c_test
+	global	myTable
 
 acs0    udata_acs		    ; reserves space for variables used
-	counter res 1		    ; reserves one bite for counter 
- 
+counter res 1		    ; reserves one bite for counter 
+	
 rst	code	0		    ; reset vector
 	goto	setup		    ; goes to code setup
 	
@@ -17,6 +18,10 @@ setup	call	setup_keypad	    ; sets up keypad
 	bcf	EECON1, CFGS	    ; point to Flash program memory  
 	bsf	EECON1, EEPGD	    ; access Flash program memory
 	call	counter_reset	    ; sets up table and counter for reading data
+	movlw	0x00
+	movwf	khigh
+	movlw	0x64
+	movwf	klow		    ; set the sample rate to 20KHz
 	goto	start		    ; goes to start of code
 	; ******* My data **
 myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0xdd, 0xca, 0xb3, 0x99, 0x7f, 0x65, 0x4b, 0x34, 0x21, 0x11, 0x06, 0x01, 0x01, 0x06, 0x11, 0x21, 0x34, 0x4b, 0x65	
@@ -24,7 +29,7 @@ myTable  db	0x7f, 0x99, 0xb3, 0xca, 0xdd, 0xed, 0xf8, 0xfd, 0xfd, 0xf8, 0xed, 0x
 start	clrf	LATC		    ; clears port c outputs	
 	clrf	LATD		    ; Clear PORTD outputs
 	movlw b'00110001'	    ; Set timer1 to 16-bit, Fosc/1:8
-	movwf	T1CON		    ; = 16MHz clock rate, approx 1sec rollover
+	movwf	T1CON		    ; = 2MHz clock rate, approx 1sec rollover
 	banksel CCPTMRS1	    ; CCPTMRS1 is in banked ram
 	bcf	CCPTMRS1, C4TSEL1   ; chooses to use timer1
 	bcf	CCPTMRS1, C4TSEL0   ; chooses to use timer1
@@ -50,12 +55,14 @@ clk_delay			    ; clock delay routine
 	return
 
 	
-int_hi	code 0x0008		    ; high vector, no low vector
+int_hi	code 0x0018		    ; high vector, no low vector
 	btfss	PIR4,CCP4IF	    ; check that this is timer1 interrupt
 	retfie	1		    ; if not then return
 	call	keypad_start	    ; calls keypad start routine
 	movff	khigh, CCPR4H	    ; from keypad start routine moves value of khigh to CCP register
 	movff	klow, CCPR4L	    ; from keypad start routine moves value of klow to CCP register
+	tstfsz	c_test, 0	    ; checks if c is pressed on the keypad, if nothing is pressed no values are read
+	call	play_song
 	tstfsz	test, 0		    ; checks if nothing is pressed on the keypad, if nothing is pressed no values are read
 	call	read		    ; reads values from table
 	bcf	PIR4,CCP4IF	    ; clear interrupt flag
